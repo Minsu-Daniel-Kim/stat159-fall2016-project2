25cc400e8a9dce4bdc79ec1c658184c131bf11a1
daniel <minsu.daniel.kim@gmail.com>
2016-10-23 19:08:20 -0700

    completed ridge and compute rsquared

--- code/scripts/ridge.R
+++ code/scripts/ridge.R
@@ -14,5 +14,21 @@ credit.train %>% nrow()
 
 train_control<- trainControl(method="cv", number=10)
 
-# train the model 
-model.lm <- train(Balance~., data=credit, trControl=train_control, method="lm")
+# train the model using caret
+model.ridge1 <- train(Balance~., data=credit.train, trControl=train_control, method="ridge")
+model.ridge1$results
+
+# prediction using model.ridge1
+model.ridge1.pred <- predict(model.ridge1, credit.test)
+rsquared(credit.test$Balance, model.ridge1.pred)
+
+# train the model using glmnet
+set.seed(1000)
+grid <- 10 ^ seq(10, -2, length = 100)
+model.ridge2.lambda <- cv.glmnet(as.matrix(select(credit.train, -Balance)), credit.train$Balance, nfolds = 10, intercept = FALSE, standardize = FALSE, lambda = grid, alpha = 0)
+model.ridge2 <- glmnet(as.matrix(select(credit.train, -Balance)), credit.train$Balance, alpha = 0, lambda = model.ridge2.lambda$lambda.min)
+
+# prediction using model.ridge1
+model.ridge2.pred <- predict(model.ridge2,newx= as.matrix(select(credit.test, -Balance)),type="response",s= model.ridge2.lambda$lambda.min)
+rsquared(credit.test$Balance, model.ridge2.pred)
+
