1d9a3f50c2b066e7a00d851585b8d74cf62c01d4
daniel <minsu.daniel.kim@gmail.com>
2016-10-23 18:46:30 -0700

    ridge cross validation added

--- code/tests/test_evaluation.R
+++ code/tests/test_evaluation.R
@@ -1,11 +1,24 @@
 library(testthat)
 source("code/functions/evaluation.R")
 
-context("Test for ols regression") 
+context("Test for evalution functions") 
 
-test_that("Sum of Squared Total", {
-  y = c(1,2,3)
-  y_mean = mean(y)
+test_that("total sum of squares", {
+  y <- c(1,2,3)
+  y_mean <- mean(y)
   expect_equal(SST(y, y_mean), 2)
 })
 
+test_that("regression sum of squares", {
+  y <- c(1,2,3)
+  y_mean = mean(y)
+  expect_equal(SSR(y, y_mean), 2)
+})
+
+test_that("r-squared", {
+  
+  y <- c(1,2,3)
+  y_estimate <- c(1, 2, 3)
+  expect_equal(rsquared(y, y_estimate), 1)
+  
+})
