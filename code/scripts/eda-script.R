23f960366dd635331d7dece0d029ecfde2e19d14
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-11-01 14:22:54 -0700

    modify eda-script

---
+++ code/scripts/eda-script.R
@@ -0,0 +1,25 @@
+library(stats)
+library(car)
+
+# Import Data
+Credit <- read.csv("data/Credit.csv", header = TRUE)
+Credit <- Credit[,2:12]
+
+# loading all function files
+files <- list.files("../functions/eda")
+
+for (i in 1:length(files)) {
+    source(paste0("../functions/eda/", files[i]))
+}
+
+# Explore Quantitative Variables
+quantitative_variables <- c("Income", "Limit", "Rating", "Cards", "Age", "Education", "Balance")
+for (i in quantitative_variables) {
+    quantitative_analysis(i)
+}
+
+# Explore Qualitative Variables
+qualitative_variables <- c("Gender", "Student", "Married", "Ethnicity", "Age")
+for (i in quantitative_variables) {
+    quantitative_analysis(i)
+}
