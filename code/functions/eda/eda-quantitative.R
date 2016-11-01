2aa3b11fa5007f70e493ea1dc174de32224fda04
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-11-01 14:37:59 -0700

    Modify eda quantitiative function

--- code/functions/eda/eda-quantitative.R
+++ code/functions/eda/eda-quantitative.R
@@ -1,33 +1,36 @@
 library(stats)
+library(dplyr)
 
 # Explore Quantitative Variables
 
 quantitative_analysis <- function(variable) {
+	# Select data
+	data <- select(Credit, variable)
 	# Minimum, Maximum, Range
-	min(Credit$variable)
-	max(Credit$variable)
-	range(Credit$variable)
+	min(data)
+	max(data)
+	range(data)
 
 	# Median, First and Third quartiles, and interquartile range (IQR)
-	median(Credit$variable)
-	quantile(Credit$variable, probs = seq(0, 1, 0.25))
-	IQR(Credit$variable)
+	median(data)
+	quantile(data, probs = seq(0, 1, 0.25))
+	IQR(data)
 
 	# Mean and Standard Deviation
-	mean(Credit$variable)
-	sd(Credit$variable)
+	mean(data)
+	sd(data)
 
 	# Histograms and boxplots
 	png(paste("images/histogram-", variable, ".png"))
 	title <- substitute(expression("Histogram of " + x), list(x = variable))
 	xlab <- substitute(expression("'" + x + "'"), list(x = variable))
-	hist(Credit$variable, main = title, xlab = xlab)
+	hist(data, main = title, xlab = xlab)
 	dev.off()
 	
 	png(paste("images/boxplot-", variable, ".png"))
 	title <- substitute(expression("Boxplot of " + x), list(x = variable))	
 	xlab <- substitute(expression(x), list(x = variable))
-	boxplot(Credit$variable, col = "lightgray", main = title, xlab = xlab)
+	boxplot(data, col = "lightgray", main = title, xlab = xlab)
 	dev.off()
 }
 
