4b0c4c3445136f3e65f0eef988a57752bcfdd772
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-11-04 17:26:29 -0700

    Update plsr code to be consistent with pcr naming style

--- code/scripts/plsr.R
+++ code/scripts/plsr.R
@@ -1,43 +1,42 @@
 library(caret)
 library(dplyr)
 library(pls)
-source("../../code/functions/evaluation.R")
+source("code/functions/evaluation.R")
 
+# import train / test datast
+load("data/credit_original_test_train.RData")
 
-# import preprocessed credit file
-credit <- read.csv("../../data/scaled-credit.csv")
-
-# split data set into 80% (training & validation) / 20% (testing)
-set.seed(1000)
-index <- createDataPartition(y = credit$Balance, p = 0.8, list = FALSE)
-credit.train <- credit[index, ]
-credit.test <- credit[-index, ]
-
-# sanity check
-credit.test %>% nrow()
-credit.train %>% nrow()
 
 # 10-fold cross validation
 model.plsr <- plsr(Balance ~ ., data = credit.train, validation = 'CV', scale = FALSE)
-model.plsr$validation$PRESS
+summary(model.plsr)
+
 
 # Plot cross-validation erros
-png("../../images/plsr-cross-validation.png")
+
 validationplot(model.plsr, val.type = "MSEP", main = "PLSR: cross-validation errors")
-dev.off()
+
+# plot combs values
+
+model.plsr.combs.plot <- plot(RMSEP(model.plsr), legendpos = "topright")
 
 # Select the "best" model
-min(model.plsr$validation$PRESS)
-plsr_best_model <- match(min(model.plsr$validation$PRESS), model.plsr$validation$PRESS)
-plsr_best_model
+model.plsr$validation$PRESS
+model.plsr.comps.min <- which.min(model.plsr$validation$PRESS)
+model.plsr.comps.min
+
 
 # Prediction for test set
-model.plsr.pred <- predict(model.plsr, ncomp = plsr_best_model, newdata=credit.test)
+model.plsr.pred <- predict(model.plsr, ncomp = model.plsr.comps.min, newdata = credit.test)
 rsquared(credit.test$Balance, model.plsr.pred)
 
+# mse
+model.plsr.mse <- get_mse(credit.test$Balance, model.plsr.pred)
+
 # Refit the model on full data set 
-model.plsr.refit <- plsr(Balance ~ ., data = credit, scale = FALSE, ncomp = plsr_best_model)
-plsr_coef <- coef(model.plsr.refit)
+model.plsr <- plsr(Balance ~ ., data = credit.original, validation = 'CV', scale = FALSE)
+model.plsr.coef <- coef(model.plsr)
+
 
 # Save info to RData
-save(model.plsr, plsr_best_model, model.plsr.pred, model.plsr.refit, plsr_coef, file = "../../data/plsr.RData")
+save(model.plsr.combs.plot, model.plsr.mse, model.plsr.coeff, file = "../../data/plsr.RData")
